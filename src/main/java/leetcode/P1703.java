6f28cd58205ce5117eff5e3d393f849035f40920
Jingyun Bian <jingbian@amazon.com>
2021-01-01 03:18:34 -0500

    Add lc 1703. Minimum Adjacent Swaps for K Consecutive Ones

---
+++ src/main/java/leetcode/P1703.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import lib.array.range_sum.RangeSumCalculator;
+
+import java.util.ArrayList;
+
+public class P1703 {
+    public int minMoves(int[] nums, int k) {
+        if (k <= 1) return 0;
+        ArrayList<Long> a = new ArrayList();
+        for (int i=0;i<nums.length;++i)
+            if (nums[i] == 1) a.add((long) i);
+
+        final int n = a.size();
+        RangeSumCalculator<Long> rsc = new RangeSumCalculator(a);
+
+        Long result = Long.MAX_VALUE;
+        int mid;
+        for (int i=0;i<n;++i){
+            if (i + k - 1 >= n) break;
+            mid = (2*i + k-1) / 2;
+            if ((k & 1) == 1){
+                result = Math.min(result, rsc.getRangeSum(mid+1, i+k-1) - rsc.getRangeSum(i,mid-1) - 2*arithmeticSum(k/2));
+            }
+            else {
+                result = Math.min(result, rsc.getRangeSum(mid+1, i+k-1) - rsc.getRangeSum(i,mid) - arithmeticSum(k/2) - arithmeticSum(k/2 - 1));
+            }
+        }
+        return Math.toIntExact(result);
+    }
+
+    private long arithmeticSum(long k){
+        return k * (k+1) / 2;
+    }
+}
---
+++ src/test/java/leetcode/P1703Test.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class P1703Test {
+
+    private P1703 underTest = new P1703();
+
+    @Test
+    public void sample1(){
+        int [] nums = new int [] {1,0,0,1,0,1};
+        int k = 2;
+        int expectedOutput = 1;
+        int result = underTest.minMoves(nums, k);
+        assertEquals(expectedOutput, result);
+    }
+
+    @Test
+    public void sample2(){
+        int [] nums = new int [] {1,0,0,0,0,0,1,1};
+        int k = 3;
+        int expectedOutput = 5;
+        int result = underTest.minMoves(nums, k);
+        assertEquals(expectedOutput, result);
+    }
+
+    @Test
+    public void sample3(){
+        int [] nums = new int [] {1,1,0,1};
+        int k = 2;
+        int expectedOutput = 0;
+        int result = underTest.minMoves(nums, k);
+        assertEquals(expectedOutput, result);
+    }
+}
