95a5b3e1c95152cf75b8f8b0085d08e63b115c1b
Jingyun Bian <jingbian@amazon.com>
2020-12-31 19:21:21 -0500

    add tests for manacher

---
+++ src/test/java/lib/ManacherTest.java
@@ -0,0 +1,29 @@
+package lib;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import lib.string.Manacher;
+
+public class ManacherTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(ManacherTest.class);
+
+    @Test
+    public void loggerTest(){
+        logger.trace("trace");
+        logger.debug("debug");
+        logger.info("info");
+        logger.warn("warn");
+        logger.error("error");
+    }
+
+    @Test
+    public void testLongestPalindrome(){
+        String st = "aabaa";
+        Manacher m = new Manacher(st);
+        System.out.println(m.getPaddedString());
+        int[] span = m.getSpan();
+        for (int i=0;i<span.length;++i) System.out.print(span[i]);
+    }
+}
