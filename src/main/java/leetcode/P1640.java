8559bd6b65b049c030011a4a57596529865b0fe0
Jingyun Bian <jingbian@amazon.com>
2021-01-01 20:40:42 -0500

    Add lc 1640. Check Array Formation Through Concatenation

---
+++ src/main/java/leetcode/P1640.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.util.HashMap;
+
+public class P1640 {
+    public boolean canFormArray(int[] arr, int[][] pieces) {
+        HashMap<Integer, Integer> mp = new HashMap();
+        for (int i=0;i<arr.length;++i) mp.put(arr[i], i);
+        for (int[] piece : pieces){
+            if (mp.get(piece[0]) == null) return false;
+            for (int i=1;i<piece.length;++i)
+                if (mp.get(piece[i]) == null || mp.get(piece[i]) != mp.get(piece[i-1]) + 1) return false;
+        }
+        return true;
+    }
+}
