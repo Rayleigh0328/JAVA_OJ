4994f7229ce652967acb940be6c350010e6a7d9c
Jingyun Bian <jingbian@amazon.com>
2022-10-20 16:52:01 -0400

    Update solution to P2439

--- src/main/java/leetcode/_24/P2439_2.java
+++ src/main/java/leetcode/_24/P2439_2.java
@@ -2,26 +2,12 @@ package leetcode._24;
 
 public class P2439_2 {
     public int minimizeArrayValue(int[] a) {
-        long cur = a[0], gap = 0, rem, delta;
-        for (int i=1;i<a.length;++i) {
-            if (a[i] <= cur) {
-                gap += cur - a[i];
-                continue;
-            }
-            if (gap >= a[i] - cur) {
-                gap -= a[i] - cur;
-                continue;
-            }
-            rem = a[i] - cur - gap;
-            if (rem % (i+1) == 0) {
-                gap = 0;
-                cur += rem / (i+1);
-            } else {
-                delta = rem / (i+1) + 1;
-                gap = (i+1) * delta - rem;
-                cur += rem / (i+1) + 1;
-            }
+        int result = -1;
+        double total = 0;
+        for (int i=0;i<a.length;++i) {
+            total += a[i];
+            result = Math.max(result, (int) Math.ceil(total / (i+1)));
         }
-        return (int) cur;
+        return result;
     }
 }
